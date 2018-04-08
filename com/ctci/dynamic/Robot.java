9b4ba5ded971333c1b8b5ef0d9ec5eee90ddbf65
mrb5960 <mandar.badave@yahoo.com>
2018-04-08 15:55:55 -0400

    added bottom up approach

--- com/ctci/dynamic/Robot.java
+++ com/ctci/dynamic/Robot.java
@@ -16,16 +16,40 @@ public class Robot {
 	void getPath(boolean[][] grid){
 		ArrayList<Cell> path = new ArrayList<>();
 		ArrayList<Cell> failed = new ArrayList<>();
-		if(getPath(grid, grid.length - 1, grid[0].length - 1, path, failed)){
+		/*if(getPathTopDown(grid, grid.length - 1, grid[0].length - 1, path, failed)){
 			for(Cell c : path){
 				System.out.println(c.row + " " + c.col);
 			}
-		}			
+		}*/
+		if(getPathBottomUp(grid, 0, 0, path, failed)){
+			for(Cell c : path){
+				System.out.println(c.row + " " + c.col);
+			}
+		}
 		else
 			System.out.println("Path does not exist");
 	}
 	
-	boolean getPath(boolean[][] grid, int row, int col, ArrayList<Cell> path, ArrayList<Cell> failed){
+	boolean getPathBottomUp(boolean[][] grid, int row, int col, ArrayList<Cell> path, ArrayList<Cell> failed){
+		if(row > grid.length - 1 || col > grid[0].length - 1 || !grid[row][col])
+			return false;
+		
+		Cell c = new Cell(row, col);
+		if(failed.contains(c))
+			return false;
+		
+		boolean isAtDest = (row == grid.length - 1) && (col == grid[0].length - 1);
+		
+		if ( isAtDest || getPathBottomUp(grid, row, col+1, path, failed) || getPathBottomUp(grid, row+1, col, path, failed)){
+			path.add(c);
+			return true;
+		}
+		
+		failed.add(c);
+		return false;
+	}
+	
+	boolean getPathTopDown(boolean[][] grid, int row, int col, ArrayList<Cell> path, ArrayList<Cell> failed){
 		if(row < 0 || col < 0 || !grid[row][col])
 			return false;
 		
@@ -39,7 +63,7 @@ public class Robot {
 		// path does not exist 
 		boolean isAtOrigin = (row == 0) && (col == 0);
 		
-		if(isAtOrigin || getPath(grid, row - 1, col, path, failed) || getPath(grid, row, col - 1, path, failed)){
+		if(isAtOrigin || getPathTopDown(grid, row - 1, col, path, failed) || getPathTopDown(grid, row, col - 1, path, failed)){
 			path.add(c);
 			return true;
 		}
@@ -50,9 +74,9 @@ public class Robot {
 
 	public static void main(String[] args) {
 		boolean[][] grid = {{true, true, true, true},
-				{true, true, true, true},
-				{true, true, true, true},
-				{true, true, true, true}};
+							{true, false, true, true},
+							{true, false, true, true},
+							{true, true, true, true}};
 		
 		Robot r = new Robot();
 		r.getPath(grid);
